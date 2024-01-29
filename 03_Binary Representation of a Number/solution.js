2f4b77d1bea01b281f31363f44ab11f88b375e24
divyam751 <mi.divyam@gmail.com>
2024-01-30 00:37:40 +0530

    Js done

---
+++ 03_Binary Representation of a Number/solution.js
@@ -0,0 +1,16 @@
+// Binary Representation of a Number
+
+let n = 17;
+let arr = [];
+let str = "";
+while (n > 0) {
+  arr.unshift(n % 2);
+  n = Math.floor(n / 2);
+}
+for (let i = 0; i < 32 - arr.length; i++) {
+  str += "" + 0;
+}
+for (let i = 0; i < arr.length; i++) {
+  str += +"" + arr[i];
+}
+console.log(str);
